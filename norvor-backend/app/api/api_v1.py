42d8b292e6a874716a524fab2df0a987fbb4392a
Apoorv Bansal <apoorvbansal@Apoorvs-MacBook-Pro.local>
2025-09-28 00:07:17 +0530

    sixth commit

--- norvor-backend/app/api/api_v1.py
+++ norvor-backend/app/api/api_v1.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter
-# This line is the corrected part. We are importing the 'router' object.
-from ..users import router as users_router
+# Import the 'router' object from our newly named 'endpoints.py' file
+from ..users.endpoints import router as users_router
 
 api_router = APIRouter()
 
-# Include the users router
+# Now we include the router object we just imported and renamed
 api_router.include_router(users_router, prefix="/users", tags=["Users"])
-
-# New feature routers will be included here in the future
-# For example:
-# api_router.include_router(crm_router, prefix="/crm", tags=["CRM"])
